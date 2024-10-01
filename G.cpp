a74ab8551be63bdd95e4cabbe78fc1789cf408c6
Andrey <andrey1010102008@gmail.com>
2024-10-02 00:06:09 +0300

    v2

--- G.cpp
+++ G.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 
-bool bypass(int n, int m, int &count, vector<vector<char>> &matrix, vector<vector<bool>> &checked) {
-	if (n < 0 || m < 0 || n >= matrix.size() || m >= matrix[n].size()) return false;
+bool bypass(int n, int m, int &count, vector<vector<char>> &matrix, vector<vector<bool>> &checked, vector<vector<bool>> &banned) {
+	if (n < 0 || m < 0 || n >= matrix.size() || m >= matrix[n].size() || banned[n][m]) return false;
 	if (checked[n][m]) return true;
 
 	checked[n][m] = true;
@@ -11,16 +11,16 @@ bool bypass(int n, int m, int &count, vector<vector<char>> &matrix, vector<vecto
 	bool ans = false;
 
 	if (matrix[n][m] == 'U') {
-		ans = bypass(n - 1, m, count, matrix, checked);
+		ans = bypass(n - 1, m, count, matrix, checked, banned);
 	}
 	else if (matrix[n][m] == 'D') {
-		ans = bypass(n + 1, m, count, matrix, checked);
+		ans = bypass(n + 1, m, count, matrix, checked, banned);
 	}
 	else if (matrix[n][m] == 'L') {
-		ans = bypass(n, m - 1, count, matrix, checked);
+		ans = bypass(n, m - 1, count, matrix, checked, banned);
 	}
 	else if (matrix[n][m] == 'R') {
-		ans = bypass(n, m + 1, count, matrix, checked);
+		ans = bypass(n, m + 1, count, matrix, checked, banned);
 	}
 
 	if (ans) {
@@ -29,6 +29,7 @@ bool bypass(int n, int m, int &count, vector<vector<char>> &matrix, vector<vecto
 	}
 	else {
 		checked[n][m] = false;
+		banned[n][m] = true;
 		return false;
 	}
 }
@@ -38,6 +39,7 @@ int main() {
 	cin >> n >> m;
 
 	vector<vector<bool>> checked(n, vector<bool>(m));
+	vector<vector<bool>> banned(n, vector<bool>(m));
 	vector<vector<char>> matrix(n, vector<char>(m));
 
 	for (int i = 0; i < n; i++) {
@@ -50,8 +52,8 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (!checked[i][j]) {
-				bypass(i, j, count, matrix, checked);
+			if (!checked[i][j] && !banned[i][j]) {
+				bypass(i, j, count, matrix, checked, banned);
 			}
 		}
 	}
